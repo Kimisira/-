b929528c0540252eb2579b5b198f2eb67fbfe3bb
Kimisira <12cm0144@gmail.com>
2016-03-26 16:47:32 +0900

    実機での実行のための微調整

--- NSKeyedArchiverSample/ViewController.swift
+++ NSKeyedArchiverSample/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
 //            print("データの保存に成功しました")
 //        }
         
+        
         //自作クラスのオブジェくをアーカイブして保存する
         //山田太郎,花子オブジェクト,(住所が同じ夫婦という設定)
         let address1 = Address(zipCode:"104-0061",state: "東京都",city: "中央区",other: "銀座一丁目")
@@ -50,18 +51,23 @@ class ViewController: UIViewController {
         let people = [tariYamada,hanakoYamada,jiroTanaka]
         
         //保存するファイルの設定
-        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as [String]
-        let filePath = String(paths[0]) + "data.dat"
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        //保存するファイルの階層
+        let filePath = String(paths).stringByAppendingString("/data.plist")
         
         //アーカイブしてファイルの設定
-        
         let successful = NSKeyedArchiver.archiveRootObject(people, toFile: filePath)
+        
+        //ちゃんと保存されているか
         if successful {
             print("データの保存に成功しました")
+        }else{
+            print("error")
         }
         
     }
-    
+    //5D5033AB-D1AB-4C88-BFB4-D597EA105121
     //アンアーカイブボタン押下時
     @IBAction func respondToUnarchveButton(sender: AnyObject) {
 //        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as Array<String>
@@ -78,17 +84,17 @@ class ViewController: UIViewController {
         
         //ファイルに保存された自作クラスのオブジェクトを復元する
         let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-        
-        let filePath = String(paths[0]) + "data.dat"
-        
-        let array = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as! Array<Person>
-        for person in array {
+        //階層してい
+        let filePath = String(paths[0]) + "/data.plist"
+        //アンアーカイブ
+        let array = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? Array<Person>
+        //要素展開
+        for person in array! {
             print(person.name)
             print(person.address.zipCode)
             print(person.address.state)
             print(person.address.city)
             print(person.address.other)
-            
         }
         
         
